d04a5c56f194392c7aa53748cd81173e55022f02
m01i0ng <maliang1066@gmail.com>
2023-03-28 13:14:37 +0800

    build: use tsup to build

--- src/node/constants/index.ts
+++ src/node/constants/index.ts
@@ -1,6 +1,6 @@
 import { join } from 'path'
 
-export const PACKAGE_ROOT = join(__dirname, '..', '..', '..')
+export const PACKAGE_ROOT = join(__dirname, '..')
 export const DEFAULT_TEMPLATE_PATH = join(PACKAGE_ROOT, 'template.html')
 
 export const CSR_ENTRY_PATH = join(PACKAGE_ROOT, 'src', 'runtime', 'csr-entry.tsx')
