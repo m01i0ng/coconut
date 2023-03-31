6bd32b51e985656d1f852e0c4c4716086899bc84
m01i0ng <maliang1066@gmail.com>
2023-03-31 10:16:10 +0800

    test: balabala e2e

--- e2e/index.test.ts
+++ e2e/index.test.ts
@@ -5,9 +5,13 @@ const siteUrl = 'http://localhost:5173'
 test('Ensure page rendering right', async ({ page }) => {
   await page.goto(siteUrl)
 
+  page.on('console', (consoleMessage) => {
+    console.log(consoleMessage.text())
+  })
+
   const res = await page.evaluate(async () => {
     const content = document.body.innerText
-    return content.includes('coconut')
+    return content.includes('')
   })
   expect(res).toBe(true)
 })
